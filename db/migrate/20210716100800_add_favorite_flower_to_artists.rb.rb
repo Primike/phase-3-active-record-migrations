160f59ad1b4d5520c9787fd5d9a73614dc37ad9d
Prince <avecillasprince@yahoo.com>
2021-08-18 17:04:05 -0400

    idk

---
+++ db/migrate/20210716100800_add_favorite_flower_to_artists.rb.rb
@@ -0,0 +1,8 @@
+# db/migrate/20210716100800_add_favorite_flower_to_artists.rb
+class AddFavoriteFlowerToArtists < ActiveRecord::Migration[6.1]
+  def change
+    add_column :artists, :favorite_flower, :string
+  end
+end
+
+#SDF
